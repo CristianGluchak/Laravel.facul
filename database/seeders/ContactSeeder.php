dc902c315879ea6c5932785395c2b68c704eab4a
CRISTIAN <cjgc4002@gmail.com>
2024-03-31 19:23:21 -0300

    feat: decodifica insert's no contactSeeder

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
 
 class ContactSeeder extends Seeder
@@ -14,16 +15,16 @@ class ContactSeeder extends Seeder
     public function run(): void
     {
         DB::table('contacts')->insert([
-            'name' => 'Jhon Does',
-            'email' => 'JhonDoes@gmail.com',
-            'telefone' => '(00)94002-8922',
+            'name' => Crypt::encryptString('Jhon Does'),
+            'email' => Crypt::encryptString('JhonDoes@gmail.com'),
+            'telefone' => Crypt::encryptString('(00)94002-8922'),
             'dt_nascimento' => '2004-09-21',
         ]);
 
         DB::table('contacts')->insert([
-            'name' => 'Jane Does',
-            'email' => 'JaneDoes@gmail.com',
-            'telefone' => '(00)94002-8922',
+            'name' => Crypt::encryptString('Jane Does'),
+            'email' => Crypt::encryptString('JaneDoes@gmail.com'),
+            'telefone' => Crypt::encryptString('(00)94002-8922'),
             'dt_nascimento' => '2000-01-01',
         ]);
     }
